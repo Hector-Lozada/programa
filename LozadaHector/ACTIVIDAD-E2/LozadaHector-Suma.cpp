6d88f1b34e403407fecc4440f4e856a7a0fa473f
Hector-Lozada <103613341+Hector-Lozada@users.noreply.github.com>
2022-05-20 10:10:44 +0200

    Actividad-E2(examen)

---
+++ LozadaHector/ACTIVIDAD-E2/LozadaHector-Suma.cpp
@@ -0,0 +1,30 @@
+//================================================
+
+
+//==> Nombre del programa: Suma de 2 numeros
+//==> Archivo : LozadaHector-Suma.cpp
+//==>Autor: Héctor Escobar
+//==>Fecha de elaboración: 2022-01-10
+//==>Fecha última actualización: 2022-05- 15
+
+
+//=================================================
+#include<iostream>
+using namespace std;
+int main()
+{
+	int he_ax, he_bx, he_cx;
+	cout<<"\n\tSUMA DE 2 NÚMEROS"<<endl;
+	cout<<"Ingrese el primer número a sumar: "; cin>>he_ax;
+	cout<<"Ingrese el segundo número a sumar: "; cin>>he_bx;
+	he_cx = he_ax+he_bx;
+	cout<<"\n\tResultado"<<endl;
+	cout<<"El resultado de la suma entre "<<he_ax<<" + "<<he_bx<<" es igual a: "<<he_cx<<endl;
+	cout<<"//================================================"<<endl;
+	cout<<"//==> Nombre del programa: Suma de 2 numeros"<<endl;
+	cout<<"//==> Archivo : LozadaHector-Suma.cpp"<<endl;
+	cout<<"//==>Autor: Héctor Escobar"<<endl;
+	cout<<"//==>Fecha de elaboración: 2022-01-10"<<endl;
+	cout<<"//==>Fecha ultima actualizaci?n: 2022-05-15"<<endl;
+	cout<<"//================================================="<<endl;
+}
